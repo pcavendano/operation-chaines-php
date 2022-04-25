66da534f161abf26a11c49def784e9971101cb41
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2022-04-25 08:36:46 -0400

    Fonction pour mélanger de cartes

--- exercice-2-php.php
+++ exercice-2-php.php
@@ -15,21 +15,104 @@
 // 6 caractères différents
 
 function password_verification($unverified_password){
-	$trim_password = ctype_space($unverified_password);
-	$error_code = 'has spaces at beginning or end';
-	for($i=0; $i < strlen($unverified_password);$i++){
-		if (strlen($unverified_password) == trim($unverified_password)) {
-			return  'has spaces at beginning or end';
-			if(strpos($unverified_password[$i], ' ') !== false){}
+	$trimed_password = trim($unverified_password);
+	$error_code = array (
+		array("espaces", "Les espaces ne sont pas permis"),
+		array("pas assez long","Le mot de passe n'est pas assez long"),
+		array("6 differents","Le mot de passe n'utilise pas 6 characteres differents")
+	);
+	$valid_password= "Mot de passe valid";
+	$valid_char = 0;
+	$one_min=0;
+	$one_maj = 0;
+	$one_num = 0;
+	$one_special = 0;
+	$one_char = 0;
+	$six_chars_array = [];
+	$pass_len = strlen($trimed_password);
+	if ($pass_len < 10){
+		return $error_code[1][1];
+	}
+	for($i=0; $i < $pass_len;$i++){
 
+		$char_val = $trimed_password[$i];
+		if ($char_val === " ") {
+			$error = $error_code[0][1];
+			$valid_password = "Mot de passe invalid";
+			return  $valid_password;
 		}
+		$iter = ord($char_val);
+		if((ord($char_val) >= 48 and ord($char_val) <= 57) || (ord($char_val) >= 65 and ord($char_val) <= 90) || (ord($char_val) >= 97 and ord($char_val) <= 122) || (ord($char_val) >= 35 and ord($char_val) <= 38) || (ord($char_val) >= 42 and ord($char_val) <= 43) || (ord($char_val) >= 58 and ord($char_val) <= 59) || (ord($char_val) == 33 ) || (ord($char_val) == 49) || (ord($char_val) == 61)){
+			//verifie la valeur unicode pour les chiffres, les lettres majuscules et minuscules
+			if((ord($char_val) >= 48 and ord($char_val) <= 57)){
+				if(!in_array(ord($char_val), $six_chars_array)){
+					array_push($six_chars_array, ord($char_val));
+				}
+				$one_num = 1;
+			}
+			if (ord($char_val) >= 65 and ord($char_val) <= 90){
+				if(!in_array(ord($char_val), $six_chars_array)){
+					array_push($six_chars_array, ord($char_val));
+				}
+				$one_maj = 1;
+			}
+			if (ord($char_val) >= 97 and ord($char_val) <= 122) {
+				if(!in_array(ord($char_val), $six_chars_array)){
+					array_push($six_chars_array, ord($char_val));
+				}
+				$one_min = 1;
+			}
+			if (ord($char_val) >= 35 and ord($char_val) <= 38){
+				if(!in_array(ord($char_val), $six_chars_array)){
+					array_push($six_chars_array, ord($char_val));
+				}
+				$one_char = 1 ;
+
+			}
+			if ((ord($char_val) >= 42 and ord($char_val) <= 43) ){
+				if(!in_array(ord($char_val), $six_chars_array)){
+					array_push($six_chars_array, ord($char_val));
+				}
+				$one_char = 1 ;
+			}
+			if(ord($char_val) >= 58 and ord($char_val) <= 59){
+				if(!in_array(ord($char_val), $six_chars_array)){
+					array_push($six_chars_array, ord($char_val));
+				}
+				$one_char = 1;
+			}
+			if (ord($char_val) == 33 ){
+				if(!in_array(ord($char_val), $six_chars_array)){
+					array_push($six_chars_array, ord($char_val));
+				}
+				$one_char = 1;
+			}
+			if (ord($char_val) == 49) {
+				if(!in_array(ord($char_val), $six_chars_array)){
+					array_push($six_chars_array, ord($char_val));
+				}
+				$one_char = 1;
+			}
+			if (ord($char_val) == 61){
+				if( !in_array(ord($char_val), $six_chars_array)){
+					array_push($six_chars_array, ord($char_val));
+				}
+				$one_char = 1;
+			}
+			$valid_char = 1;
+		}else{
+			$valid_password = "Mot de passe invalid";
+			return  $valid_password;
+		}
+
+
+	}
+	if ($valid_char == 1 && $one_num == 1 && $one_maj == 1 && $one_min == 1 && count($six_chars_array) >= 6){
+		return $valid_password;
+	}else{
+		return $error_code[2][1];
 	}
 
-
-
-	$unverified_password = $trim_password == true? "true":"false";
-	echo $error_code;
-	return $error_code;
 };
 
 
@@ -41,23 +124,25 @@ $password_5 = "0e&a22222A2";
 $password_6 = "2e&a22222A2";
 $password_7 = "0&a22223A";
 
-echo '<div>'."Exercice 2".'</div>' ;
+//Execution du programme
+
+echo '<div>'."Exercice 2 ".'</div>' ;
+$letreuni =ord("Z");
+echo '<div>'.$letreuni.'</div>' ;
 echo "<p>";
 echo 'Chaîne du mot de passe contrôlée : '.$password_1 .' -> valide </p>';
-echo '<div class = "password">'.'Validation ave la fonction password_verification -> Le mot de passe est '.password_verification($password_1)."<br>"."<br>"."</div>";
-echo 'Chaîne du mot de passe contrôlée : '.$password_2 .' -> valide </p>';
-echo '<div class = "password">'.'Validation ave la fonction password_verification -> Le mot de passe est '.password_verification($password_2)."<br>"."<br>"."</div>";
-echo 'Chaîne du mot de passe contrôlée : '.$password_3 .' -> valide </p>';
-echo '<div class = "password">'.'Validation ave la fonction password_verification -> Le mot de passe est '.password_verification($password_3)."<br>"."<br>"."</div>";
-echo 'Chaîne du mot de passe contrôlée : '.$password_4 .' -> valide </p>';
-echo '<div class = "password">'.'Validation ave la fonction password_verification -> Le mot de passe est '.password_verification($password_4)."<br>"."<br>"."</div>";
+echo '<div class = "password">'.'Validation ave la fonction password_verification -> '.password_verification($password_1)."<br>"."<br>"."</div>";
+echo 'Chaîne du mot de passe contrôlée : '.$password_2 .' -> non valide </p>';
+echo '<div class = "password">'.'Validation ave la fonction password_verification ->'.password_verification($password_2)."<br>"."<br>"."</div>";
+echo 'Chaîne du mot de passe contrôlée : '.$password_3 .' -> non valide </p>';
+echo '<div class = "password">'.'Validation ave la fonction password_verification -> '.password_verification($password_3)."<br>"."<br>"."</div>";
+echo 'Chaîne du mot de passe contrôlée : '.$password_4 .' -> non valide </p>';
+echo '<div class = "password">'.'Validation ave la fonction password_verification -> '.password_verification($password_4)."<br>"."<br>"."</div>";
 echo 'Chaîne du mot de passe contrôlée : '.$password_5 .' -> valide </p>';
-echo '<div class = "password">'.'Validation ave la fonction password_verification -> Le mot de passe est '.password_verification($password_5)."<br>"."<br>"."</div>";
-echo 'Chaîne du mot de passe contrôlée : '.$password_6 .' -> valide </p>';
-echo '<div class = "password">'.'Validation ave la fonction password_verification -> Le mot de passe est '.password_verification($password_6)."<br>"."<br>"."</div>";
-echo 'Chaîne du mot de passe contrôlée : '.$password_7 .' -> valide </p>';
-echo '<div class = "password">'.'Validation ave la fonction password_verification -> Le mot de passe est '.password_verification($password_7)."<br>"."<br>"."</div>";
+echo '<div class = "password">'.'Validation ave la fonction password_verification -> '.password_verification($password_5)."<br>"."<br>"."</div>";
+echo 'Chaîne du mot de passe contrôlée : '.$password_6 .' -> non valide </p>';
+echo '<div class = "password">'.'Validation ave la fonction password_verification -> '.password_verification($password_6)."<br>"."<br>"."</div>";
+echo 'Chaîne du mot de passe contrôlée : '.$password_7 .' -> non valide </p>';
+echo '<div class = "password">'.'Validation ave la fonction password_verification -> '.password_verification($password_7)."<br>"."<br>"."</div>";
 echo "</p>";
-
-
 ?>
