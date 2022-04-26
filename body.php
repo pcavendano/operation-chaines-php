2d0b476c6eaaa66db2c80ce6126af5c5425210ee
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2022-04-25 21:07:28 -0400

    Ajout de pied de page

--- body.php
+++ body.php
@@ -3,25 +3,11 @@
 
 <?php
 
-
-include ('exercice2-2.php');
 include ('exercice-2-php.php');
+include ('exercice2-2.php');
+include ('pied_de_page.php');
 
-
-
-
-function afficherPiedDePage($societe, $annee) {
-	$societe = "Pedro Contreras Avendano";
-	$annee = "2022";
+afficherPiedDePage("Pedro Contreras Avendano", "2022");
 	?>
 
-	<footer>
-		<p>&copy;<?php echo $societe." ".$annee; ?></p>
-	</footer>
-	<?php
-}
-?>
-<script type="text/javascript">
-    console.log("Hello");
-</script>
-</body>
+
---
+++ pied_de_page.php
@@ -0,0 +1,16 @@
+<?php
+
+function afficherPiedDePage($societe, $annee)
+{
+
+	echo '<footer>
+	<p>&copy;';
+	echo "$societe $annee";
+		echo '</p>
+</footer>
+<script type="text/javascript">
+    console.log("Hello");
+</script>
+</body>';
+}
+?>
